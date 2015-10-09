0a102144d74bfd051da84fae9a1f8c2c903b4274
Marko Budal <markobudal@Markos-MacBook-Pro.local>
2015-10-09 14:55:03 +0200

    First Commit

---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+use_frameworks!
+
+target 'testAlamofire' do
+pod 'Alamofire', '~> 3.0.0-beta.3'
+end
+
+target 'testAlamofireTests' do
+
+end
+
+target 'testAlamofireUITests' do
+
+end
+
--- testAlamofire/ViewController.swift
+++ testAlamofire/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
@@ -20,6 +21,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendJson(sender: UIButton) {
+        let parameters = ["aa":"1"]
+        
+        //postJSON("http://telemetry.sandbox.axa.baselinetelematics.com/T2/inbound.php?imei = " +mMacAddress.replace(":", ""), packetJson);
+        //http://collector.staging.promutuel.baselinetelematics.com/1.0/t2
+
+        
+        let http = "http://telemetry.sandbox.axa.baselinetelematics.com/T2/inbound.php?imei=84EB182B97CD"
+    
+//        Alamofire.request(.POST, http, parameters: parameters, encoding: .JSON).responseJSON {
+//            (res) -> Void in
+//            
+//            print("Res: \(res.description)")
+//            print("Res: \(res.description)")
+//            print("Res: \(res.description)")
+//            
+//            }
+
+        let fileURL = NSBundle.mainBundle().URLForResource("myJson", withExtension: "json")
+            print(fileURL)
+        
+        Alamofire.upload(.POST, http, file: fileURL!).responseJSON {
+                        (res) -> Void in
+            
+                        print("Res: \(res.description)")
+                        print("Res: \(res.description)")
+                        print("Res: \(res.description)")
+                        
+                        }
+        }
+
+
 
 }
+
+
 
