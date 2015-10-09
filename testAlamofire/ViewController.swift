0343200452bfca5f06b34473fa807853a94995df
Marko Budal <markobudal@markos-macbook-pro.local>
2015-10-09 12:56:55 +0200

    Initial Commit

---
+++ testAlamofire/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testAlamofire
+//
+//  Created by Marko Budal on 09/10/15.
+//  Copyright © 2015 Marko Budal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
